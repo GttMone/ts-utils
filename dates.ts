f798cc5cbf09cca53caee8cad56fe7e053bd17c7
GttMone <66485019+GttMone@users.noreply.github.com>
2024-12-02 19:22:22 +0200

    Create dates.ts

---
+++ dates.ts
@@ -0,0 +1,11 @@
+export function getMinutesBetweenDates(pastDate: Date, currentDate: Date) {
+    return Math.round(((currentDate.valueOf() - pastDate.valueOf()) / 1000) / 60);
+}
+
+export function getSecondsBetweenDates(pastDate: Date, currentDate: Date) {
+    return Math.abs((currentDate.getTime() - pastDate.getTime()) / 1000);
+}
+
+export function getHoursBetweenDates(pastDate: Date, currentDate: Date) {
+    return Math.abs(currentDate.valueOf() - pastDate.valueOf()) / 36e5;
+}
